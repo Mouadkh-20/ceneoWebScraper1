3d1f5a047f535e965a5f7e9ec0afa3101af9ec82
Mouadkh-20 <mouadkhouider15@gmail.com>
2025-05-12 15:28:19 +0200

    12 may

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route("/")
+@app.route("/<name>")
+def greeting(name="World"):
+    return f"Hello{name}!"
+    return render_template("index.html", name=name)
